a490bd6654cadbf6b0e32289d59f729ace87e847
Smithfn1012 <58006468+Smithfn1012@users.noreply.github.com>
2020-07-05 10:23:56 -0500

    add fetch request to commentActions.js

--- src/actions/commentActions.js
+++ src/actions/commentActions.js
@@ -1,3 +1,16 @@
 export const LOAD_COMMENTS = 'LOAD_COMMENTS';
 export const ADD_COMMENT_ANIMAL = 'ADD_COMMENT_ANIMAL';
 export const ADD_COMMENT_PLANT ='ADD_COMMENT_PLANT'
+
+export const getComments = () => {
+    return dispatch => {
+      return fetch('https://localhost:3001/api/v1/comments')
+      .then(res=>res.json())
+      .then(comments => {
+        dispatch({
+          type: LOAD_COMMENTS,
+          payload: comments
+        })
+      }).catch(console.error)
+    }
+}
