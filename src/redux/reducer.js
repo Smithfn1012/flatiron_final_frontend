b87c177dec8611d4530cf45d767b11d7d1af4a00
Smithfn1012 <58006468+Smithfn1012@users.noreply.github.com>
2020-07-05 09:46:48 -0500

    add const reducer and case LOAD_ANIMALS to reducer

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -9,3 +9,10 @@ const initialState = {
     comments: [],
     loading: false
 };
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case LOAD_ANIMALS: {
+            return { ...state, animals: action.payload };
+        }
+    }
+}
