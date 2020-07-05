1713352a1ea36a5c2fd074e33e13be1a0d918dec
Smithfn1012 <58006468+Smithfn1012@users.noreply.github.com>
2020-07-05 10:26:47 -0500

    add const addCommentAnimal & fetch request

--- src/actions/commentActions.js
+++ src/actions/commentActions.js
@@ -18,3 +18,28 @@ export const getComments = () => {
 let animalPics = ['./images/profile-arctic-fox.jpg', './images/profile-dumbo.jpg', './images/profile-emink.jpg', './images/profile-grizzlybear.jpg']
 
 let randomAnimal = animalPics[Math.floor(Math.random() * animalPics.length)]
+
+export const addCommentAnimal = (comment) => {
+    return dispatch => {
+      return fetch(`https://localhost:3001/api/v1/animals/${comment.animal_id}/comments`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Accept: 'application/json'
+        },
+        body: JSON.stringify({
+          name: comment.name,
+          comment: comment.comment,
+          animal_id: comment.animal_id,
+          plant_id: null,
+          animal_img: randomAnimal
+        })
+      }).then(res=>res.json())
+        .then(comment=>{
+          dispatch({
+            type: ADD_COMMENT_ANIMAL,
+            payload: comment
+          })
+        }).catch(console.error)
+    }
+  }
