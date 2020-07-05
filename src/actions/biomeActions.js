0669ee0454f6b86f09623fefaf3e4e829dae3bad
Smithfn1012 <58006468+Smithfn1012@users.noreply.github.com>
2020-07-05 10:20:46 -0500

    add export const getBiomes to biomeActions.js

--- src/actions/biomeActions.js
+++ src/actions/biomeActions.js
@@ -1,3 +1,20 @@
 export const LOAD_BIOMES = "LOAD_BIOMES";
 export const CHANGE_BIOME = "CHANGE_BIOME";
 export const START_ADDING_BIOMES = "START_ADDING_BIOMES";
+
+export const getBiomes = () => {
+    return dispatch => {
+        dispatch({
+            type: START_ADDING_BIOMES
+        });
+        return fetch("https://localhost:3001/api/v1/biomes")
+        .then(res => res.json())
+        .then(biomes => {
+            dispatch({
+                type: LOAD_BIOMES,
+                payload: biomes
+            });
+        })
+        .catch(console.error);
+    };
+};
