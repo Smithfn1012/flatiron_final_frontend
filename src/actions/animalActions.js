4a4d644c2f053ab91e6a7184b0110ba824520c3b
Smithfn1012 <58006468+Smithfn1012@users.noreply.github.com>
2020-07-05 10:14:09 -0500

    add export const addAnimal to animalActions.js

--- src/actions/animalActions.js
+++ src/actions/animalActions.js
@@ -20,3 +20,32 @@ export const changeAnimal = (animal) => {
         payload: animal
     }
 }
+export const addAnimal = (animal) => {
+    return (dispatch) => {
+        return fetch('https://localhost:3001/api/v1/animals', {
+            method: 'POST',
+            headers: {
+                'Content-Type': "application/json",
+                Accept: "application/json"
+            },
+            body: JSON.stringify({
+                species: animal.species,
+                lifespan: animal.lifespan,
+                size: animal.size,
+                prey: animal.prey,
+                predators: animal.predators,
+                location: animal.predators,
+                other_info: animal.other_info,
+                image: animal.image,
+                endangered: animal.endangered,
+                biome_id: animal.biome_id
+            })
+        }).then(res => res.json())
+          .then(animal => {
+              dispatch({
+                  type: ADD_ANIMAL,
+                  payload: animal
+              })
+          }).catch(console.error)
+    }
+}
