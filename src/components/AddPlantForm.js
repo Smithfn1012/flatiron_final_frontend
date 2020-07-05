7d26704215d94ff7dfeb2e0f7e6b54378733b385
Smithfn1012 <58006468+Smithfn1012@users.noreply.github.com>
2020-07-05 10:44:12 -0500

    handle component and forms for AddPlantForm.js

---
+++ src/components/AddPlantForm.js
@@ -0,0 +1,111 @@
+import React, { Component } from 'react';
+import { Button, Form} from 'semantic-ui-react'
+import { connect } from 'react-redux'
+import { addPlant } from '../actions/plantActions'
+
+const options = [
+  { key: 'a', text: 'Aquatic', value: 1 },
+  { key: 'tu', text: 'Tundra', value: 2 },
+  { key: 'dt', text: 'Desert', value: 3 },
+  { key: 'r', text: 'Rainforest', value: 4 },
+  { key: 'g', text: 'Grasslands', value: 5 },
+  { key: 'ta', text: 'Taiga', value: 6 },
+  { key: 'de', text: 'Deciduous Forest', value: 7 }
+]
+
+class AddPlantForm extends Component {
+
+  state = {
+    species: "",
+    size: "",
+    predators: "",
+    location: "",
+    seed_distribution: "",
+    other_info: "",
+    image: "",
+    biome_id: ""
+  }
+
+  changeHandler = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  biomeChangeHandler = (e, data) => {
+    this.setState({
+      biome_id: data.value
+    })
+  }
+
+  submitHandler = (e) => {
+    e.preventDefault()
+    let newPlant = this.state
+    this.props.addPlant(newPlant)
+    alert(`${this.state.species} was created!`)
+
+    this.setState({
+      species: "",
+      size: "",
+      predators: "",
+      location: "",
+      seed_distribution: "",
+      other_info: "",
+      image: "",
+      biome_id: ""
+    })
+  }
+
+  render(){
+    return(
+
+      <Form className="forms" onSubmit={this.submitHandler} >
+        <h1 className="form-header">Add Plant</h1>
+        <hr />
+
+        <Form.Field>
+          <Form.Select name="biome_id" fluid label='Biome' options={options} placeholder='Biome' onChange={(e, data) => this.biomeChangeHandler(e, data)} />
+        </Form.Field>
+
+        <Form.Field>
+          <label>Species</label>
+          <input type="text" name="species" placeholder="species" value={this.state.species} onChange={this.changeHandler} />
+        </Form.Field>
+
+        <Form.Field>
+        <label>Size</label>
+        <input type="text" name="size" placeholder="size" value={this.state.size} onChange={this.changeHandler} />
+        </Form.Field>
+
+        <Form.Field>
+        <label>Predators</label>
+        <input type="text" name="predators" placeholder="predators" value={this.state.predators} onChange={this.changeHandler} />
+        </Form.Field>
+
+        <Form.Field>
+        <label>Location</label>
+        <input type="text" name="location" placeholder="location" value={this.state.location} onChange={this.changeHandler} />
+        </Form.Field>
+
+        <Form.Field>
+          <label>Seed Distribution</label>
+          <input type="text" name="seed_distribution" placeholder="Seed Distribution" value={this.state.seed_distribution} onChange={this.changeHandler} />
+        </Form.Field>
+
+        <Form.Field>
+        <label>Interesting Fact</label>
+        <textarea name="other_info" placeholder="Facts" value={this.state.other_info} onChange={this.changeHandler} />
+        </Form.Field>
+
+        <Form.Field>
+          <label>Image</label>
+          <input type="text" name="image" placeholder="image" value={this.state.image} onChange={this.changeHandler} />
+        </Form.Field>
+
+        <Form.Field control={Button}>Submit</Form.Field>
+
+      </Form>
+    )
+  }
+}
+export default connect(null, {addPlant})(AddPlantForm);
