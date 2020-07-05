d7a30376eb503a46fdd0446aa7f6c482124363f5
Smithfn1012 <58006468+Smithfn1012@users.noreply.github.com>
2020-07-05 10:07:18 -0500

    add const getAnimals and create fetch request

--- src/actions/animalActions.js
+++ src/actions/animalActions.js
@@ -1,3 +1,16 @@
 export const LOAD_ANIMALS = 'LOAD_ANIMALS'
 export const CHANGE_ANIMAL = 'CHANGE_ANIMAL'
 export const ADD_ANIMAL = 'ADD_ANIMAL'
+
+export const getAnimals = () => {
+    return (dispatch) => {
+        return fetch('https://localhost:3001/api/v1/animals')
+        .then(res => resp.json())
+        .then(animals => {
+            dispatch({
+                type: LOAD_ANIMALS,
+                payload: animals
+            })
+        }).catch(console.error)
+    }
+}
