a7b6b4485ae781ed9908be3990fc29c40e512c57
Smithfn1012 <58006468+Smithfn1012@users.noreply.github.com>
2020-07-05 11:10:16 -0500

    create selectableAnswer component for quiz

---
+++ src/components/SelectableAnswer.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+
+class SelectableAnswer extends Component {
+  state = {
+    selected: false
+  }
+
+  handleClick = (value) => {
+    if(!this.props.disabled) {
+      this.props.onClick(value)
+      this.setState({
+        selected: true
+      })
+    }
+  }
+  color = (top, choice) => {
+    if (top === choice) {
+      return choice? "cg" : "cr"
+    } else {
+      return "cw"
+    }
+  }
+
+  render(){
+    const { selected } = this.state;
+    const { choice, disabled, topSelected } = this.props;
+    return(
+      <div className="quiz__options ac" onClick={()=> this.handleClick(choice.answer)}>
+        <div className={`${disabled && !selected ? this.color(topSelected, choice.answer) : ""} ${selected ? (choice.answer ? "cg" : "cr") : "cw"}`}>
+          {choice.item}
+        </div>
+      </div>
+    )
+  }
+}
+export default SelectableAnswer;
